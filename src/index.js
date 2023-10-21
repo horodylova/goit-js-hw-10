bf195394430b3c3b47ab1524bd8cb8647508ca98
svitlana-horodylova <horodylova.sv@gmail.com>
2023-10-21 17:19:00 +0100

    first_fetch_list_of_breeds

--- src/index.js
+++ src/index.js
@@ -2,26 +2,32 @@ import axios from "axios";
 
 axios.defaults.headers.common["x-api-key"] = "live_Tw25qqZGTgRzQUEFXOfLqmslZZwx9HXbjDePGI0ZChRJtxUAscxAhnVEo8KUClDE";
 
+const breedSelect = document.querySelector(".breed-select");
+const maxHeight = 200;
+const loader = document.querySelector('.loader');
+const error = document.querySelector('.error');
 
-// import PLoader from 'ploader';
-// const loader = new PLoader({
-//     container: "loader-container",
-//   });
-  
-const breedSelect = document.querySelector("breed-select");
-breedSelect.id = "breedSelect";
+loader.hidden = true;
+error.hidden = true;
 
-// fetchBreeds(){
-//     fetch(`https://api.example.com/data?option=${selectedValue}`)
-//     .then(response => response.json())
-//     .then(data => {
-//       const resultElement = document.getElementById("result");
-//       resultElement.innerHTML = `Выбранная опция: ${selectedValue}, Результат: ${data.result}`;
-//     })
-//     .catch(error => {
-//       console.error("Ошибка при выполнении запроса:", error);
-//     });
-//     });
-// }
+const BASE_URL = 'https://api.thecatapi.com/v1';
 
+function fetchBreeds() {
+  axios.get(`${BASE_URL}/breeds`)
+    .then(response => {
+      const data = response.data;
 
+      data.forEach(breed => {
+        const option = document.createElement("option");
+        option.value = breed.id; 
+        option.text = breed.name;
+        breedSelect.appendChild(option);
+      });
+    })
+    .catch(err => {
+      
+      error.hidden = false;
+    });
+}
+
+fetchBreeds();
