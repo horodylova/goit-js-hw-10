3b64f4caa78916734bf998c08e96c8bd126f5bad
svitlana-horodylova <horodylova.sv@gmail.com>
2023-10-22 09:58:28 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,7 +12,21 @@ error.hidden = true;
 
 const BASE_URL = 'https://api.thecatapi.com/v1';
 
-function fetchBreeds() {
+function openSelect() {
+  breedSelect.classList.add('open'); 
+  breedSelect.style.maxHeight = '200px';
+  breedSelect.style.overflowY = 'auto';
+}
+
+function closeSelect() {
+  breedSelect.classList.remove('open');
+  breedSelect.style.maxHeight = 'none';
+  breedSelect.style.overflowY = 'visible';
+}
+
+function fetchBreeds(onSuccess, onError) {
+  closeSelect();
+
   axios.get(`${BASE_URL}/breeds`)
     .then(response => {
       const data = response.data;
@@ -23,11 +37,21 @@ function fetchBreeds() {
         option.text = breed.name;
         breedSelect.appendChild(option);
       });
+
+      if (onSuccess) {
+        onSuccess();
+      }
     })
     .catch(err => {
-      
+      console.error("Ошибка при загрузке списка пород кошек:", err);
       error.hidden = false;
+
+      if (onError) {
+        onError();
+      }
     });
 }
 
+
 fetchBreeds();
+
