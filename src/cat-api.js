693c98754812716282dec37b91776c6d3f834423
svitlana-horodylova <horodylova.sv@gmail.com>
2023-10-23 15:42:51 +0100

    mistakes

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,15 +1,8 @@
 import axios from 'axios';
 import Notiflix from 'notiflix'; 
-import { BASE_URL } from './index';
-
-const error = document.querySelector('.error');
-const loader = document.querySelector('.loader');
-
-loader.hidden = true;
-error.hidden = true;
+const BASE_URL = 'https://api.thecatapi.com/v1';
 
 export async function fetchBreeds(breedSelect, onSuccess, onError) {
-  Notiflix.Loading.standard('Loading breeds, please wait...'); 
   
     axios.get(`${BASE_URL}/breeds`)
       .then(response => {
@@ -33,18 +26,19 @@ export async function fetchBreeds(breedSelect, onSuccess, onError) {
         if (onError) {
           onError();
         }
-        Notiflix.Notify.failure('Oops! Something went wrong. Please try again.'); 
       });
   }
 
 export async function fetchCatByBreed(breedId) {
-  Notiflix.Loading.standard('Loading cat info, please wait...'); 
-
-  const BASE_URL = 'https://api.thecatapi.com/v1';
+  // Notiflix.Loading.standard('Loading cat info, please wait...'); 
 
   return axios
     .get(`${BASE_URL}/images/search?breed_ids=${breedId}&include_breeds=true`)
-    .then(response => response.data[0])
+    .then(response => {
+      console.log(response.data);  
+      return response.data[0];
+  })
+     
     .then(catData => {
       Notiflix.Loading.remove(); 
       return catData;
--- src/index.js
+++ src/index.js
@@ -1,26 +1,19 @@
+// import axios from "axios";
 import Notiflix from 'notiflix';
 import { fetchBreeds, fetchCatByBreed } from './cat-api';
 
 const breedSelect = document.querySelector(".breed-select");
 const error = document.querySelector('.error');
-const loader = document.querySelector('.loader');
 
-loader.hidden = true;
 error.hidden = true;
-
-let isLoading = false; 
 
 export const BASE_URL = 'https://api.thecatapi.com/v1';
 
 breedSelect.addEventListener('change', () => {
-  if (isLoading) {
-    return; 
-  }
-
-  isLoading = true; 
-  Notiflix.Loading.standard('Loading data, please wait...');
-
   const selectedBreedId = breedSelect.value;
+
+  // Показать сообщение о загрузке
+  Notiflix.Loading.standard('Loading...');
 
   if (selectedBreedId) {
     fetchCatByBreed(selectedBreedId)
@@ -66,17 +59,104 @@ breedSelect.addEventListener('change', () => {
         catInfoWrapper.appendChild(catImage);
         catInfoContainer.appendChild(catInfoWrapper);
 
+        // Скрыть сообщение о загрузке
         Notiflix.Loading.remove();
-        error.hidden = true;
-        isLoading = false; 
       })
       .catch(error => {
-        Notiflix.Notify.failure('Oops! Something went wrong! Try reloading the page!');
-        Notiflix.Loading.remove();
+        console.error("Ошибка при загрузке информации о кошке:", error);
         error.hidden = false;
-        isLoading = false;
+        // Показать сообщение об ошибке
+        Notiflix.Notify.failure('Failed to load cat information');
+        // Скрыть сообщение о загрузке
+        Notiflix.Loading.remove();
       });
   }
 });
 
 fetchBreeds(breedSelect);
+
+// import axios from "axios";
+
+// axios.defaults.headers.common["x-api-key"] = "live_Tw25qqZGTgRzQUEFXOfLqmslZZwx9HXbjDePGI0ZChRJtxUAscxAhnVEo8KUClDE";
+
+// import { fetchBreeds, fetchCatByBreed } from './cat-api';
+// import Notiflix from 'notiflix';
+// // import SlimSelect from 'slim-select';
+
+// const breedSelect = document.querySelector(".breed-select");
+
+// // new SlimSelect({
+// //   select: breedSelect,
+// //   dataAttributeValue: 'data-color', 
+// // });
+
+// const loader = document.querySelector('.loader');
+// const error = document.querySelector('.error');
+
+// loader.hidden = true;
+// error.hidden = true;
+
+// export const BASE_URL = 'https://api.thecatapi.com/v1';
+
+// breedSelect.addEventListener('change', () => {
+//   const selectedBreedId = breedSelect.value;
+
+//   if (selectedBreedId) {
+//     loader.hidden = false;
+    
+//     fetchCatByBreed(selectedBreedId)
+//     .then(catData => {
+//       const catInfoContainer = document.querySelector('.cat-info');
+//       catInfoContainer.innerHTML = '';
+
+//       const catInfoWrapper = document.createElement("div");
+//       catInfoWrapper.style.display = "flex";
+
+//       const catImage = document.createElement("img");
+//       catImage.src = catData.url;
+//       catImage.alt = catData.name;
+//       catImage.style.width = "50%";
+//       catImage.style.height = "auto";
+
+//       catImage.style.maxHeight = '300px';
+//       catImage.style.width = 'auto';
+//       catImage.style.border = '5px double gray';
+//       catImage.style.flex = "1";
+
+//       catImage.loading = "lazy";
+
+//       const textInfoContainer = document.createElement("div");
+//       textInfoContainer.style.flex = "2";  
+//       textInfoContainer.style.display = "flex";
+//       textInfoContainer.style.flexDirection = "column";
+//       textInfoContainer.style.alignItems = "flex-start";
+
+//       const catName = document.createElement("h2");
+//       catName.textContent = catData.breeds[0].name;
+//       catName.style.margin = "0";
+
+//       const catDescription = document.createElement("p");
+//       catDescription.textContent = `Description: ${catData.breeds[0].description}`;
+
+//       const catTemperament = document.createElement("p");
+//       catTemperament.textContent = `Temperament: ${catData.breeds[0].temperament}`;
+
+//       textInfoContainer.appendChild(catName);
+//       textInfoContainer.appendChild(catDescription);
+//       textInfoContainer.appendChild(catTemperament);
+
+//       catInfoWrapper.appendChild(textInfoContainer);
+//       catInfoWrapper.appendChild(catImage);
+//       catInfoContainer.appendChild(catInfoWrapper);
+
+//       loader.hidden = true;
+//     })
+//     .catch(error => {
+//       console.error("Ошибка при загрузке информации о кошке:", error);
+//       error.hidden = false;
+//       loader.hidden = true;
+//     });
+//   }
+// });
+
+// fetchBreeds(breedSelect);
